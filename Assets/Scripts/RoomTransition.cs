2bb61f083f30cf65b223f36fc33d9a9ddb7137ea
Nstjacques <Nstjacques15bf@gmail.com>
2017-11-28 14:27:07 -0500

    Room Transition Scripts and Prefabs
    Camera Prefab, Screen Transition Volume Prefab, and Scripts

---
+++ Assets/Scripts/RoomTransition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour {
+
+	public GameObject Room1;
+	public GameObject Room2;
+
+	public GameObject trigger;
+
+	private bool usedBefore = true;
+
+	// Use this for initialization
+	void Start () {
+		Room1.SetActive(true);
+		Room2.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider trigger) {
+		if (usedBefore == true) {
+			Debug.Log ("If you see this and the camera fails shit's broke");
+			Room1.SetActive (false);
+			Room2.SetActive (true);
+			usedBefore = false;
+		} else {
+			Room1.SetActive (true);
+			Room2.SetActive (false);
+			usedBefore = true;
+		}
+	}
+}
