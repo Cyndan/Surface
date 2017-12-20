d0fab38ad766665f0274e111df3e8240977cc7fb
LaptopJV <water_in_the_dark@yahoo.com>
2017-12-20 13:46:04 -0500

    Death Animation

---
+++ Assets/Scripts/DeathPuff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPuff : MonoBehaviour 
+{
+	private float toDie;
+	private float deathTimer = 1.0f;
+
+	void Start () 
+	{
+		toDie = deathTimer + Time.time;
+	}
+	
+
+	void Update () 
+	{
+		if (Time.time > toDie)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class PlayerLife : MonoBehaviour 
 {
 	public PlayerRespawn respawnScript;
+	public GameObject deathPuff;
+	public GameObject tracer;
 	private bool hit;
 
 	void Start ()
 	{
 		respawnScript = GameObject.FindWithTag("GameController").GetComponent<PlayerRespawn>();
+		tracer = GameObject.FindGameObjectWithTag("Tracer");
 	}
 
 	void Update ()
 	{
+		tracer.transform.position = gameObject.transform.position;
+
 		if (gameObject.transform.position.y < -20.0f && !hit)
 		{
 			hit = true;
 			respawnScript.deathCount++;
 			Destroy(gameObject);
+			Instantiate(deathPuff, tracer.transform);
 		}
 	}
 
@@ -29,6 +35,7 @@ public class PlayerLife : MonoBehaviour
 			hit = true;
 			respawnScript.deathCount++;
 			Destroy(gameObject);
+			Instantiate(deathPuff, tracer.transform.position, new Quaternion(0, 0, 0, 0));
 		}
 
 		if (other.tag == "Respawn")
@@ -43,6 +50,7 @@ public class PlayerLife : MonoBehaviour
 		{
 			hit = true;
 			respawnScript.deathCount++;
+			Instantiate(deathPuff, tracer.transform.position, new Quaternion(0, 0, 0, 0));
 			Destroy(gameObject);
 		}
 	}
